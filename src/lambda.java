4749c1f04f70b250f90d1c2230e5185db5650403
yntymakabc <yntymak.almazbekuulu1@gmail.com>
2025-03-25 17:17:35 +0600

    Create lambda.java

---
+++ src/lambda.java
@@ -0,0 +1,15 @@
+class lambada {
+    public static void main(String[] args) {
+        MathOperations add = (a,b) -> a + b;
+        MathOperations sub = (a,b) -> a - b;
+        MathOperations mult = (a, b) -> a * b;
+        System.out.println(add.operations(2, 4));
+        System.out.println(sub.operations(5, 1));
+        System.out.println(mult.operations(5, 2));
+    }
+}
+
+
+interface MathOperations{
+    int operations(int a, int b);
+}
