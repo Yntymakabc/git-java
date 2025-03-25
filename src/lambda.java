e2447043461121697b8430190d4dfba153b3e6ae
yntymakabc <yntymak.almazbekuulu1@gmail.com>
2025-03-25 21:33:45 +0600

    Update lambda.java

--- src/lambda.java
+++ src/lambda.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.List;
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+
 class lambada {
     public static void main(String[] args) {
         MathOperations add = (a,b) -> a + b;
@@ -14,4 +23,78 @@ class lambada {
 
 interface MathOperations{
     int operations(int a, int b);
+}
+
+
+class SecondTask{
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 15, 22, 33, 40, 55);
+        List<Integer> oddNumber = new ArrayList<>();
+
+        Predicate<Integer> isOdd = number -> number % 2 != 0;
+
+    
+        for (Integer num : numbers) {
+            if (isOdd.test(num)) {
+                oddNumber.add(num);
+            }
+        }
+        System.out.println(oddNumber);
+    }
+}
+
+class ThirdTask{
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David");
+        names.sort((a, b) -> b.compareTo(a));
+
+        System.out.println("Sorted Names: " + names);
+    }
+}
+
+
+class Four{
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("hello", "java", "lambda");
+        List<String> newWords = new ArrayList<>();
+        
+        UpperReverse UR = word -> new StringBuilder(word.toUpperCase()).reverse().toString();
+
+        for(String word2: words){
+            newWords.add(UR.reversedandSmt(word2));
+        } 
+        System.out.println(newWords);   
+    }
+}
+
+interface UpperReverse{
+    String reversedandSmt(String word);
+}
+
+
+class Five{
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("New York", "London", "Tokyo", "Berlin");
+        Consumer<String> printCity = city -> System.out.println(city);
+        cities.forEach(printCity);
+    }
+  
+
+}
+
+class Six{
+    public static void main(String[] args) {
+        List<String> namess = Arrays.asList("Alice", "Bob", "Charlie", "David");
+        namess.forEach(name -> System.out.println(name));
+        namess.forEach(System.out::println);
+    }
+}
+
+class Severn{
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> maxFunction = (x, y) -> x > y ? x : y;
+        BiFunction<Integer, Integer, Integer> minFunction = (x, y) -> x < y ? x : y;
+        System.out.println("max " + maxFunction.apply(1,2));
+        System.out.println("min " + minFunction.apply(4, 6));
+    }
 }
