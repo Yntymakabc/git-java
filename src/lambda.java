1283edf7ade5eab2a4977c0aa611154488582d73
yntymakabc <yntymak.almazbekuulu1@gmail.com>
2025-03-25 17:33:46 +0600

    Update lambda.java

--- src/lambda.java
+++ src/lambda.java
@@ -3,9 +3,11 @@ class lambada {
         MathOperations add = (a,b) -> a + b;
         MathOperations sub = (a,b) -> a - b;
         MathOperations mult = (a, b) -> a * b;
+        MathOperations div = (a, b) -> a / b;
         System.out.println(add.operations(2, 4));
         System.out.println(sub.operations(5, 1));
         System.out.println(mult.operations(5, 2));
+        System.out.println(div.operations(4, 2));
     }
 }
 
