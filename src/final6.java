2e90bfed54dd8367b7eac3a0bd5073b64982ef39
Yntymak <yntymak.almazbekuulu1@gmail.com>
2025-05-11 23:45:27 +0600

    j

--- src/final6.java
+++ src/final6.java
@@ -68,3 +68,58 @@
 // // interface Abc{
 // //     public boolean Check(String word);
 // // }
+
+// import java.util.function.Function;
+
+// public class final6{
+//     public static void main(String[] args) {
+//         Function<String, Integer> f = (String sentence)->{
+//             String[] word = sentence.split(",");
+//             return word.length;
+//         };
+//         System.out.println(f.apply("this, time for"));
+//     }
+// }
+
+// import java.util.ArrayList;
+// import java.util.Arrays;
+
+// public class final6 {
+//     public static void main(String[] args) {
+//         ArrayList<String> arr = new ArrayList<>(Arrays.asList("yntymak", "akush", "aibek"));
+        
+//         Big b =(a)->{
+//             for(String i:arr){
+//                 System.out.println(i.toUpperCase());
+//             }
+//         };
+//         b.makeBig(arr);
+//     }
+// }
+// interface Big  {
+// void makeBig(ArrayList<String> arr);
+    
+// };
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class final6 {
+    public static void main(String[] args) {
+        ArrayList arr = new ArrayList<>(Arrays.asList(3,4,2,4,5,6,7,8));
+        Akmat A =(list)->{
+            ArrayList <Integer> arrNew = new ArrayList<>();
+            for(int i:list){
+                if(i%2==0){
+                    arrNew.add(i);
+
+                }
+            }
+            return arrNew;
+        };
+        System.out.println(A.evens(arr));
+    }
+}
+interface Akmat{
+    public ArrayList<Integer> evens(ArrayList<Integer> arr);
+}
